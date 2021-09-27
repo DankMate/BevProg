ec4d201b3d6025abb8a0523d8f7dbd667dab3894
Máté Pál Vajda <vajdamate0204@gmail.com>
2021-09-27 19:38:17 +0200

    Első három drill

--- drillek/drill_3/drill_3.cpp
+++ drillek/drill_3/drill_3.cpp
@@ -14,30 +14,29 @@ int main()
 	char friend_sex = '0';
 	cout <<"Enter an f if the friend is female and m if the friend is male: ";
 	cin >> friend_sex;
-	if (friend_sex='m')
+	if (friend_sex=='m')
 	{
-		cout <<"If you see " <<friend_name<< " please ask him to call me.";
+		cout <<"If you see " <<friend_name<< " please ask him to call me.\n";
 	}
 	else
 	{
-		cout <<"If you see " <<friend_name<< " please ask her to call me.";
+		cout <<"If you see " <<friend_name<< " please ask her to call me.\n";
 	}
 	cout << "Please enter your age: ";
 	int age=0;
 	cin >> age;
-	cout << "I hear you just had a birthday and you are "<< age<< " years old\n" ;
+	cout << "I hear you just had a birthday and you are "<< age<< " years old.\n" ;
 	
 	if (age < 0 || age > 110)
 		simple_error("you're kidding\n");
-	 
 	if (age < 12)
 		cout<< "Next year you will be " << age+1 <<".\n";
-	else if (age = 17)
+	else if (age == 17)
 		cout<<"Next year you will be able to vote.\n";
 	else if (age > 70)
 		cout<<"I hope you are enjoying retirement.\n";    
 
 
-	cout <<"Yours sincerely,\n\n \nVajda Máté\n";
+	cout <<"Yours sincerely,\n\n\nVajda Máté\n";
 	return 0;
 }
